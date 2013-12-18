be84305a16dec546ddb5874b161006de9e1bd403
Nathan Jervis <mirhagk@gmail.com>
2013-12-18 13:06:55 -0500

    Added GitSharp
    Still MANY errors, slowly fixing them. This project is pure C#, which
    makes it more promising than the other git libraries

---
+++ Windows8Git/GitSharp.Core/Util/Win8Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace GitSharp.Core.Util
+{
+    public static class Win8Util
+    {
+        public static DateTimeOffset GetLastModified(this StorageFile file)
+        {
+                var t = file.GetBasicPropertiesAsync().AsTask();
+                t.Wait();
+                return t.Result.DateModified;
+        }
+        public static long GetLastModifiedMilli(this StorageFile file)
+        {
+            return GetLastModified(file).ToMillisecondsSinceEpoch();
+        }
+    }
+}
